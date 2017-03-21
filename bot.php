5694d00b0c9c0cf36d8b566f301f5a1cca7cfff1
DESKTOP-ETQACBR\Supakij <first50@windowslive.com>
2017-03-21 16:14:47 +0700

    V1.0

--- bot.php
+++ bot.php
@@ -12,7 +12,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			 = $event['message']['text'];
+			$text = $event['message']['text'];
 			if ($text == "สวัสดี")
 			{
 				$text2 = "สวัสดีครับ";
