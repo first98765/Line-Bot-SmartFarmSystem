7951f1b4d29d9f804afa2ce81cb8682ba0861db5
DESKTOP-ETQACBR\Supakij <first50@windowslive.com>
2017-03-21 15:06:23 +0700

    hello

--- bot.php
+++ bot.php
@@ -12,19 +12,15 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'];
+			$text = $event["Hello"]['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-			if($text == "Hi")
-			{
-				$messages = ["Hello I'm Bot"];
-			}
-
-			// Build message to reply back
-			// $messages = [
-			// 	'type' => 'text',
-			// 	'text' => $text
-			// ];
+			
+			Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
