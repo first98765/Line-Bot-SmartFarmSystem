7d571d228916867e4311b8c822f8c68f8c539f8d
DESKTOP-ETQACBR\Supakij <first50@windowslive.com>
2017-03-21 15:29:24 +0700

    1

--- bot.php
+++ bot.php
@@ -12,7 +12,10 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = "Hello";
+			if('message' == "Hi")
+			{
+				$text = "Hello";
+			}
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
