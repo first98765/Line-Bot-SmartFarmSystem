aeecf4e1d1962ac81a977dd6bbfbd7a6e2a13989
DESKTOP-ETQACBR\Supakij <first50@windowslive.com>
2017-03-21 15:14:41 +0700

    2

--- bot.php
+++ bot.php
@@ -13,13 +13,17 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
+			if($text == "Hi")
+			{
+				$text2 = $event['Hello I am bot.']
+			}
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text2
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
